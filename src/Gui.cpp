9f69faf0dfa667a3d3a0d590f3f8df5016db3d74
MFRSiam <mfrforhad1@gmail.com>
2023-07-03 17:09:03 +0600

    New Net V0.0.4 Added GUI Class

---
+++ src/Gui.cpp
@@ -0,0 +1,8 @@
+//
+// Created by M.F.R.Siam on 7/3/2023.
+//
+
+#include "Gui.hpp"
+
+namespace Observer {
+} // Observer
---
+++ src/Gui.hpp
@@ -0,0 +1,16 @@
+//
+// Created by M.F.R.Siam on 7/3/2023.
+//
+
+#ifndef NEURALNETWORK_PROJECT_GUI_HPP
+#define NEURALNETWORK_PROJECT_GUI_HPP
+
+namespace Observer {
+
+    class Gui {
+
+    };
+
+} // Observer
+
+#endif //NEURALNETWORK_PROJECT_GUI_HPP
